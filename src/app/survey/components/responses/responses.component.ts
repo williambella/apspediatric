1373fd93f5e65d825694acbb3abc78023d8c4d03
Bruno Serrão <brunosserrao@gmail.com>
2020-03-23 00:57:49 +0100

    preparing dynamic form values from Survey form to submit

--- src/app/survey/components/responses/responses.component.ts
+++ src/app/survey/components/responses/responses.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { GroupService } from '@appointment/services/group.service';
 import { QuestiontService } from '@appointment/services/question.service';
 import { Group } from '@appointment/models/group';
@@ -19,7 +19,7 @@ export class ResponsesComponent implements OnInit, OnDestroy, OnChanges {
   @Input() patients: Array<Patient>;
 
   groups: Array<Group>;
-  questions: Array<Question>;
+  questions: Array<Array<Question>> = new Array<[]>();
 
   private readonly formArrayName = 'questions';
 
@@ -33,32 +33,34 @@ export class ResponsesComponent implements OnInit, OnDestroy, OnChanges {
   }
 
   ngOnInit() {
-    // this.formGroup = this.formBuilder.group({
-    //   formArrayQuestions: this.formBuilder.group({})
-    // });
-
-    // this.loadGroups();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes && changes.formGroup.currentValue) {
-      this.formGroup.addControl(this.formArrayName, new FormArray([]));
-
-      this.loadGroups();
-
-      // if (changes && changes.contacts.currentValue) {
-      //   (changes.contacts.currentValue as Array<Contact>).map((contact: Contact) => this.addContact(contact));
-      // } else {
-      //   this.addContact();
-      // }
-    }
   }
 
   ngOnDestroy(): void {
     this.arraySubscriptions.map((subscription: Subscription) => subscription.unsubscribe());
   }
 
-  formSubmit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes && changes.formGroup.currentValue) {
+      this.formGroup.addControl(this.formArrayName, this.formBuilder.group({}));
+      this.loadGroups();
+    }
+  }
+
+  formSubmit(): void {
+    console.log(this,this.formGroup.value);
+    if (this.formGroup.valid) {
+      // TODO: Create or Update A Survey
+    }
+  }
+
+  questionsFromGroup(idGroup: string): FormGroup {
+   return this.formQuestionsArray.get(idGroup) as FormGroup;
+  }
+
+  questionByGroupId(id: string): Question {
+    return this.questions.reduce((aq: Array<Question>) => aq)
+    .find((q: Question) => q.id === id);
+  }
 
   private loadGroups(): void {
     const groupsSubscription: Subscription = this.groupService.findAll()
@@ -76,46 +78,36 @@ export class ResponsesComponent implements OnInit, OnDestroy, OnChanges {
   private loadQuestions(group: Group): void {
     const questionsSubscription: Subscription = this.questiontService.findAllByGroupId(group.id)
     .subscribe((questions: Array<Question>) => {
-      this.questions = questions;
+      if (questions.length) {
+        this.formQuestionsArray.addControl(group.id, new FormArray([]));
 
-      const formGroup: FormGroup = new FormGroup({});
-      // this.formGroup.addControl(group.id, new FormArray([]));
-      // this.formQuestionsArray.push(this.formBuilder.group)
+        this.questions.push(questions);
 
-      this.questions.map((question: Question) => this.addQuestion(group, question, formGroup));
+        questions.map((question: Question) => {
+          const formGroup: FormGroup = this.formBuilder.group({
+            id: [null],
+            questionId: [question.id],
+            idGroup: [question.idGroup],
+            idType: [null],
+            question: [question.question],
+            response: [null, Validators.required],
+            patientId: [null]
+          });
+
+          (this.formQuestionsArray.get(group.id) as FormArray).push(formGroup);
+        });
+      }
     });
 
     this.arraySubscriptions = [...this.arraySubscriptions, questionsSubscription];
   }
 
-  addQuestion(group: Group, question: Question, formGroup: FormGroup): void {
-    const formControl: FormControl = new FormControl({
-      id: [null],
-      questionId: [question.id],
-      idGroup: [group.id],
-      idType: [null],
-      question: [null],
-      response: [null],
-      patientId: [null]
-    });
-
-    (formGroup.get(group.id) as FormArray).push(formControl);
-    // this.formQuestionsArray.get()
-    // this.formQuestionsArray.push(this.formBuilder.group({
-    //   id: [question ? question.id : null],
-    //   questionId: [question ? question.questionid],
-    //   idGroup: [],
-    //   idType: [],
-    //   question: [],
-    //   response: [],
-    //   patientId: []
-    // }));
-
-    this.formQuestionsArray.push(formGroup);
+  get formQuestionsArray(): FormGroup {
+    return this.formGroup.get(this.formArrayName) as FormGroup;
   }
 
-  get formQuestionsArray(): FormArray {
-    return this.formGroup.get(this.formArrayName) as FormArray;
+  get getFormArrayName(): string {
+    return this.formArrayName;
   }
 
 }
