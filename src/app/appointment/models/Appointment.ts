8f1ff12e321e22c3db7ebc007ead89e53471d3be
Lucas Alves <lucasalvessm@outlook.com>
2020-04-22 18:54:40 -0300

    fix build

--- src/app/appointment/models/Appointment.ts
+++ src/app/appointment/models/Appointment.ts
@@ -3,6 +3,7 @@ import { Procedure } from './Procedure';
 
 export interface Appointment {
     id?: string;
+    date?: string;
     description: string;
     patientId: string;
     treatments: Array<Treatment>;
